336c272f0d4cea4aa8c014498897d79009630ad3
Tanguy-Supinfo <303082@supinfo.com>
2019-10-22 19:07:08 +0200

    Button test

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ Frame.resizable(width=False, height=False)
 canvas = Canvas(Frame,width=1000,height=650,bg="white")
 canvas.pack()
 
+def myfonc(nameBtn):
+    print("pressed : " + nameBtn)
+
 base_folder = os.path.dirname(__file__)
 image_path = os.path.join(base_folder, 'imgs/background.png')
 
@@ -20,8 +23,13 @@ canvas.create_text(850,260,fill="black",font="null 20",text="Votre Pion")
 
 canvas.create_text(850,380,fill="black",font="null 20",text="Ordinateur")
 
+button1 = Button(Frame, text = "5", anchor = W, bg="#15BDAC", command=lambda: myfonc("5"))
+button1.configure(width=21, height=10, activebackground = "#33B5E5", relief = FLAT)
+button1_window = canvas.create_window(212, 235, anchor=NW, window=button1)
+
 Frame.mainloop()
 
+#15BDAC
 
 # canvas.create_rectangle(50,50,600,600,width=2)
 
