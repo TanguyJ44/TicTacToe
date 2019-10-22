53f82ca5342c6966323e2e1ea69a422d14e81530
Tanguy-Supinfo <303082@supinfo.com>
2019-10-22 19:37:53 +0200

    Fianl button case

--- main.py
+++ main.py
@@ -9,9 +9,6 @@ Frame.resizable(width=False, height=False)
 canvas = Canvas(Frame,width=1000,height=650,bg="white")
 canvas.pack()
 
-def myfonc(nameBtn):
-    print("pressed : " + nameBtn)
-
 base_folder = os.path.dirname(__file__)
 image_path = os.path.join(base_folder, 'imgs/background.png')
 
@@ -23,11 +20,43 @@ canvas.create_text(850,260,fill="black",font="null 20",text="Votre Pion")
 
 canvas.create_text(850,380,fill="black",font="null 20",text="Ordinateur")
 
-button1 = Button(Frame, text = "5", anchor = W, bg="#15BDAC", command=lambda: myfonc("5"))
-button1.configure(width=21, height=10, activebackground = "#33B5E5", relief = FLAT)
-button1_window = canvas.create_window(212, 235, anchor=NW, window=button1)
+def myfonc(nameBtn):
+    print("Case : ", nameBtn)
+
+case1 = Button(Frame, text = "1", anchor = W, bg="#15BDAC", command=lambda: myfonc(1))
+case2 = Button(Frame, text = "2", anchor = W, bg="#15BDAC", command=lambda: myfonc(2))
+case3 = Button(Frame, text = "3", anchor = W, bg="#15BDAC", command=lambda: myfonc(3))
+case4 = Button(Frame, text = "4", anchor = W, bg="#15BDAC", command=lambda: myfonc(4))
+case5 = Button(Frame, text = "5", anchor = W, bg="#15BDAC", command=lambda: myfonc(5))
+case6 = Button(Frame, text = "6", anchor = W, bg="#15BDAC", command=lambda: myfonc(6))
+case7 = Button(Frame, text = "7", anchor = W, bg="#15BDAC", command=lambda: myfonc(7))
+case8 = Button(Frame, text = "8", anchor = W, bg="#15BDAC", command=lambda: myfonc(8))
+case9 = Button(Frame, text = "9", anchor = W, bg="#15BDAC", command=lambda: myfonc(9))
+
+case1.configure(width=21, height=10, activebackground = "#33B5E5", relief = FLAT)
+case2.configure(width=21, height=10, activebackground = "#33B5E5", relief = FLAT)
+case3.configure(width=21, height=10, activebackground = "#33B5E5", relief = FLAT)
+case4.configure(width=21, height=10, activebackground = "#33B5E5", relief = FLAT)
+case5.configure(width=21, height=10, activebackground = "#33B5E5", relief = FLAT)
+case6.configure(width=21, height=10, activebackground = "#33B5E5", relief = FLAT)
+case7.configure(width=21, height=10, activebackground = "#33B5E5", relief = FLAT)
+case8.configure(width=21, height=10, activebackground = "#33B5E5", relief = FLAT)
+case9.configure(width=21, height=10, activebackground = "#33B5E5", relief = FLAT)
+
+case1_render = canvas.create_window(39, 60, anchor=NW, window=case1)
+case2_render = canvas.create_window(213, 60, anchor=NW, window=case2)
+case3_render = canvas.create_window(389, 60, anchor=NW, window=case3)
+case4_render = canvas.create_window(39, 237, anchor=NW, window=case4)
+case5_render = canvas.create_window(213, 235, anchor=NW, window=case5)
+case6_render = canvas.create_window(389, 235, anchor=NW, window=case6)
+case7_render = canvas.create_window(39, 413, anchor=NW, window=case7)
+case8_render = canvas.create_window(213, 413, anchor=NW, window=case8)
+case9_render = canvas.create_window(389, 413, anchor=NW, window=case9)
 
 Frame.mainloop()
+
+
+
 
 #15BDAC
 
