934781f46a892770c1ec6f2afbbfa550d33e91fb
Tanguy-Supinfo <303082@supinfo.com>
2019-10-22 14:28:04 +0200

    fresh table

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from tkinter import *
 
-fenetre = Tk()
+Fenetre = Tk()
 
-champ_label = Label(fenetre, text="Tic Tac Toe - Supinfo")
+zone_dessin = Canvas(Fenetre,width=1000,height=650,bg="white",bd=8)
+zone_dessin.pack()
 
-champ_label.pack()
+zone_dessin.create_rectangle(50,50,600,600,width=2)
 
-fenetre.mainloop()
+zone_dessin.create_line(50,230,600,230,fill="red",width=2)
+zone_dessin.create_line(50,420,600,420,fill="red",width=2)
+
+zone_dessin.create_line(230,50,230,600,fill="red",width=2)
+zone_dessin.create_line(420,50,420,600,fill="red",width=2)
+
+Fenetre.mainloop()
